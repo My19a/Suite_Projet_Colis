cfb7739abde2500ec19bc0dcf9e521a99a0b3aa2
Fares-bna <fares.bennani@etu.u-paris.fr>
2026-06-18 21:25:34 +0200

    correction footer hors syntaxe html

--- src/public/views/partials/footer.php
+++ src/public/views/partials/footer.php
@@ -15,15 +15,8 @@
 </footer>
 
 <?php if (!empty($avecTutoriel)) require __DIR__ . '/tutoriel.php'; ?>
+
+<?php require __DIR__ . '/chatbot.php'; ?>
+
 </body>
 </html>
-
-<?php 
-// Sécurité pour cibler le dossier partials qui est dans views/
-if (file_exists(__DIR__ . '/partials/chatbot.php')) {
-    include __DIR__ . '/partials/chatbot.php';
-} else {
-    // Si votre footer est dans un sous-dossier (ex: views/admin/footer.php), on remonte d'un cran
-    include __DIR__ . '/../partials/chatbot.php';
-}
-?>
